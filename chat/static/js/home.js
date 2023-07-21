b8a0df7b1b61827543e0787fe8f5e2eefd9b2bd7
waisodum <123014654+waisodum@users.noreply.github.com>
2023-07-21 16:22:55 +0530

    home.js

--- chat/static/js/home.js
+++ chat/static/js/home.js
@@ -1,6 +1,4 @@
 const socket = io("http://127.0.0.1:3000");
-
-
 var messagecontainer=document.querySelector('.container');
 var messagecontent=document.querySelector('.msg');
 var sent=document.getElementById('form');
@@ -12,6 +10,8 @@ messagelement.innerText=message;
 messagelement.classList.add('msg');
 messagelement.classList.add(position);
 messagecontainer.append(messagelement)
+var h=messagecontainer.clientHeight;
+messagecontainer.scrollBy(0,h);
 }
 
 var form=document.getElementById('form');
@@ -21,6 +21,7 @@ form.addEventListener('submit',(e)=>{
     var msgdata= text.value;
     append(msgdata,"right");
     socket.emit('send',msgdata);
+    text.value=" ";
 })
 socket.emit('new-user-join', name);
 socket.on('user-joined',(name)=>{
